a6ebdfe39c22a59a6d189c8714fd8641e2241b7e
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-07-10 00:12:25 +0500

    Added comments

--- countUpAndDown.ts
+++ countUpAndDown.ts
@@ -4,10 +4,11 @@ const countUpAndDown = n => {
     console.log(i);
   }
   console.log("Going Down: ");
-
   for (let i = n; i >= 0; i--) {
     console.log(i);
   }
+
+  // executes in 2n time or roughly Order of n or O(n)
 };
 
 (() => {
